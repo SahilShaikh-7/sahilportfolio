13b84207b628a8b5d46d00c3e6bc97600f1ddf0d
Sahil Shaikh <sahil68shaikh68@gmail.com>
2026-05-20 19:14:00 +0530

    Update neural-network.tsx

--- components/3d/neural-network.tsx
+++ components/3d/neural-network.tsx
@@ -16,7 +16,7 @@ function InteractiveNetwork() {
   const { mouse, viewport, camera } = useThree()
   const { resolvedTheme } = useTheme()
 
-  const particleCount = 180
+  const particleCount = 230
   const maxDistance = 3.5
 
   const mousePosition3D = useRef(new THREE.Vector3())
